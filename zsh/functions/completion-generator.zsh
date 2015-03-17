a0fe6051efdcb407c834a4a8214bbb06f2a13b04
Benjamin Saiz <benj.saiz@gmail.com>
2015-03-17 17:43:16 +0100

    add compgen

---
+++ zsh/functions/completion-generator.zsh
@@ -0,0 +1,22 @@
+# Parse getopt-style help texts for options
+# and generate zsh(1) completion functions.
+# http://github.com/RobSis/zsh-completion-generator
+
+OUTPUTDIR="${ZSH}completions"
+SCRIPT_SOURCE=${0:A:h}
+
+function gencomp() {
+    if [ -z "$1" ]; then
+        echo "Usage: gencomp program [--argument-for-help-text]"
+        echo
+        return 1
+    fi
+
+    help=--help
+    if [ -n "$2" ]; then
+        help=$2
+    fi
+
+    $1 $help 2>&1 | /usr/bin/python $SCRIPT_SOURCE/help2comp.py $1 > $OUTPUTDIR/_$1 || ( rm -f $OUTPUTDIR/_$1 &&\
+        echo "No options found for '$1'." )
+}
