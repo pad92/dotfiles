b95e58975e55d75c6d17c7ada002387e480ee6b0
pad <git@depad.fr>
2016-04-01 14:40:22 +0200

    add check

--- zsh/functions/http_crawler.zsh
+++ zsh/functions/http_crawler.zsh
@@ -1,17 +1,28 @@
 crawler () {
-   if [[ $# -eq 1 ]]; then
-       SITEMAPS=$(curl -s $1 | xml2 | grep '/loc=' | sed 's/.*=//' 2>/dev/null)
-       for URL in `echo $SITEMAPS`; do
-           if [[ $URL =~ '\.xml$' ]]; then
-               crawler $URL
-           else
-               HTTP_CODE=$(curl -s -w "%{http_code}" $URL -o /dev/null 2>/dev/null)
-               echo "$HTTP_CODE - $URL"
-           fi
-       done
+    BIN_DEPS='xml2 curl'
+    # === CHECKS ===
+    for BIN in `echo $BIN_DEPS`; do
+        which $BIN 1>/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "Error: Required binarie could not be found: $BIN"
+            return 1
+        fi
+    done
 
-   else
-       echo usage: crawler domain.ltd/sitemap.xml >&2
-       return 1
-   fi
+    if [[ $# -eq 1 ]]; then
+        SITEMAPS=$(curl -s $1 | xml2 | grep '/loc=' | sed 's/.*=//' 2>/dev/null)
+        for URL in `echo $SITEMAPS`; do
+            if [[ $URL =~ '\.xml$' ]]; then
+                crawler $URL
+            else
+                HTTP_CODE=$(curl -s -w "%{http_code}" $URL -o /dev/null 2>/dev/null)
+                echo "$HTTP_CODE - $URL"
+            fi
+        done
+        return 0
+
+    else
+        echo usage: crawler domain.ltd/sitemap.xml >&2
+        return 1
+    fi
 }
