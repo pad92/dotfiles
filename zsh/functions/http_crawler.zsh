dce277a3cfe1087308eafc1d96a4ff6e3a54624b
pad <git@depad.fr>
2016-04-13 20:37:12 +0200

    follow 3xx

--- zsh/functions/http_crawler.zsh
+++ zsh/functions/http_crawler.zsh
@@ -10,12 +10,12 @@ http_crawler () {
     done
 
     if [[ $# -eq 1 ]]; then
-        SITEMAPS=$(curl -s $1 | xml2 | grep '/loc=' | sed 's/.*=//' 2>/dev/null)
+        SITEMAPS=$(curl -s $1 -L | xml2 | grep '/loc=' | sed 's/.*=//' 2>/dev/null)
         for URL in `echo $SITEMAPS`; do
             if [[ $URL =~ '\.xml$' ]]; then
                 http_crawler $URL
             else
-                HTTP_CODE=$(curl -s -w "%{http_code}" $URL -o /dev/null 2>/dev/null)
+                HTTP_CODE=$(curl -s -w "%{http_code}" $URL -o /dev/null -L 2>/dev/null)
                 echo "$HTTP_CODE - $URL"
             fi
         done
