a4de0a8291add9eb04bef0f9395a25fdea4957d9
pad <git@depad.fr>
2016-06-08 16:07:10 +0200

    add docker alias with sudo

--- zsh/functions/lol.zsh
+++ zsh/functions/lol.zsh
@@ -1,3 +1,3 @@
 onsenbatlescouilles () {
-    mplayer ~/Music/On\ s\'en\ bat\ les\ couilles-XoDY9vFAaG8.m4a
+    mplayer -loop 0 ~/Music/On\ s\'en\ bat\ les\ couilles-XoDY9vFAaG8.m4a
 }
---
+++ zsh/init/docker.zsh
@@ -0,0 +1,14 @@
+BIN_DEPS=('docker' 'sudo')
+STATUS=0
+# === CHECKS ===
+for BIN in $BIN_DEPS; do
+    which $BIN 1>/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+		(( STATUS = $STATUS + 1 ))
+        echo "$BIN fail"
+    fi
+done
+
+if [ $STATUS -eq 0 ]; then
+	alias docker='sudo docker'
+fi
