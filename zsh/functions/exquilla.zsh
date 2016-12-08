48e27979a35fb4c984bf9e293c27db2462e963e0
pad <git@depad.fr>
2016-12-08 11:37:50 +0100

    fix perms, add exquilla functions

---
+++ zsh/functions/exquilla.zsh
@@ -0,0 +1,10 @@
+exquilla_gen () {
+    if [ "$#" -ne 1 ] ; then
+        echo "Usage: $0 user@domain.ltd" >&2
+    else
+        MAIL_ADDRESS=$1
+        DATE_EXPIRE=`date +%Y-%m-%d -d "2 year"`
+        LICENCE_MD5=`echo -n "EX1,$1,$DATE_EXPIRE,356B4B5C" | md5sum | cut -d' ' -f1`
+        echo "EX1,$1,$DATE_EXPIRE,$LICENCE_MD5"
+    fi
+}
