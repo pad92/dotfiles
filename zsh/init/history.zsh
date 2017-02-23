f424f08c1fe49adc0fad4513c6c23394ef6a0e50
Pascal Armand <pascal.armand@cloud-temple.com>
2017-02-23 17:41:00 +0100

    fix history

--- zsh/init/history.zsh
+++ zsh/init/history.zsh
@@ -6,19 +6,27 @@ fi
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt BANG_HIST # Treat the '!' character specially during expansion.
-setopt APPEND_HISTORY # # Allow multiple terminal sessions to all append to one zsh command history
-setopt EXTENDED_HISTORY # Write the history file in the ':start:elapsed;command' format.
-setopt INC_APPEND_HISTORY # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY # Share history between all sessions.
+setopt BANG_HIST              # Treat the '!' character specially during expansion.
+setopt APPEND_HISTORY         # Allow multiple terminal sessions to all append to one zsh command history
+setopt EXTENDED_HISTORY       # Write the history file in the ':start:elapsed;command' format.
+setopt INC_APPEND_HISTORY     # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY          # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST # Expire a duplicate event first when trimming history.
-setopt HIST_IGNORE_DUPS # Do not record an event that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS # Delete an old recorded event if a new event is a duplicate.
-setopt HIST_FIND_NO_DUPS # Do not display a previously found event.
-setopt HIST_REDUCE_BLANKS # Remove extra blanks from each command line being added to history
-#setopt HIST_IGNORE_SPACE # Do not record an event starting with a space.
-setopt HIST_SAVE_NO_DUPS # Do not write a duplicate event to the history file.
-#setopt HIST_VERIFY # Do not execute immediately upon history expansion.
-#setopt HIST_BEEP # Beep when accessing non-existent history.
+setopt HIST_IGNORE_DUPS       # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS   # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_FIND_NO_DUPS      # Do not display a previously found event.
+setopt HIST_REDUCE_BLANKS     # Remove extra blanks from each command line being added to history
+#setopt HIST_IGNORE_SPACE     # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS      # Do not write a duplicate event to the history file.
+#setopt HIST_VERIFY           # Do not execute immediately upon history expansion.
+#setopt HIST_BEEP             # Beep when accessing non-existent history.
 
 alias history-stat="history . | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
+
+# Show history
+case $HIST_STAMPS in
+  "mm/dd/yyyy") alias history='fc -fl 1' ;;
+  "dd.mm.yyyy") alias history='fc -El 1' ;;
+  "yyyy-mm-dd") alias history='fc -il 1' ;;
+  *) alias history='fc -l 1' ;;
+esac
