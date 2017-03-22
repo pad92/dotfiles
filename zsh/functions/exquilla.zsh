9e8e016c35cd77db44a6cd7082439702dda231c4
pad <git@depad.fr>
2017-03-22 13:38:48 +0100

    fix

--- zsh/functions/exquilla.zsh
+++ zsh/functions/exquilla.zsh
@@ -3,7 +3,7 @@ exquilla_gen () {
         echo "Usage: $0 user@domain.ltd" >&2
     else
         MAIL_ADDRESS=$1
-        DATE_EXPIRE=`date +%Y-%m-%d -d "2 year"`
+        DATE_EXPIRE=`date +%Y-%m-%d -d "1 year"`
         LICENCE_MD5=`echo -n "EX1,$1,$DATE_EXPIRE,356B4B5C" | md5sum | cut -d' ' -f1`
         echo "EX1,$1,$DATE_EXPIRE,$LICENCE_MD5"
     fi
