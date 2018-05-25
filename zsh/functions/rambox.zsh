d39a8a0d87508ba88f9bf811f5729b361915bab1
Pascal <pascalito@gmail.com>
2018-05-25 19:35:26 +0200

    fix for bash

--- zsh/functions/rambox.zsh
+++ zsh/functions/rambox.zsh
@@ -8,9 +8,9 @@ rambox_update(){
 
     rambox_CURRENT=$(rpm -aq | grep -i rambox | cut -d'-' -f2)
     rambox_LATEST=$(curl -s https://api.github.com/repos/saenzramiro/rambox/releases/latest)
-    rambox_LATEST_VERSION=$(echo ${rambox_LATEST} | grep -m 1 '"name"'                          | cut -d '"' -f 4)
-    rambox_RPM=$(echo ${rambox_LATEST}            | grep -E 'browser_download_url(.*)\.rpm'     | cut -d '"' -f 4 | grep -v ia32)
-    rambox_DEB=$(echo ${rambox_LATEST}            | grep -E 'browser_download_url(.*)\.deb'     | cut -d '"' -f 4 | grep -v ia32) 
+    rambox_LATEST_VERSION=$(echo -e ${rambox_LATEST} | grep -m 1 '"name"'                          | cut -d '"' -f 4)
+    rambox_RPM=$(echo -e ${rambox_LATEST}            | grep -E 'browser_download_url(.*)\.rpm'     | cut -d '"' -f 4 | grep -v ia32)
+    rambox_DEB=$(echo -e ${rambox_LATEST}            | grep -E 'browser_download_url(.*)\.deb'     | cut -d '"' -f 4 | grep -v ia32) 
 
     if [ "${rambox_LATEST_VERSION}" != "${rambox_CURRENT}" ]; then
         echo "upgrade rambox ${rambox_CURRENT} to ${rambox_LATEST_VERSION}"
