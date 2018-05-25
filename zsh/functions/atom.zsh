36e3436f9d8c3848085c2cc5586caf24f1d99e05
pad <git@depad.fr>
2018-05-25 19:33:04 +0200

    fix for bash

--- zsh/functions/atom.zsh
+++ zsh/functions/atom.zsh
@@ -8,9 +8,9 @@ atom_update(){
 
     ATOM_CURRENT=`atom -v | grep '^Atom' | awk '{print $NF}' 2>/dev/null`
     ATOM_LATEST=$(curl -s https://api.github.com/repos/atom/atom/releases/latest)
-    ATOM_LATEST_VERSION=$(echo ${ATOM_LATEST} | grep -m 1 '"name"'                          | cut -d '"' -f 4)
-    ATOM_RPM=$(echo ${ATOM_LATEST}            | grep -E 'browser_download_url(.*)\.rpm'     | cut -d '"' -f 4)
-    ATOM_DEB=$(echo ${ATOM_LATEST}            | grep -E 'browser_download_url(.*)\.deb'     | cut -d '"' -f 4)
+    ATOM_LATEST_VERSION=$(echo -e ${ATOM_LATEST} | grep -m 1 '"name"'                          | cut -d '"' -f 4)
+    ATOM_RPM=$(echo -e ${ATOM_LATEST}            | grep -E 'browser_download_url(.*)\.rpm'     | cut -d '"' -f 4)
+    ATOM_DEB=$(echo -e ${ATOM_LATEST}            | grep -E 'browser_download_url(.*)\.deb'     | cut -d '"' -f 4)
 
     if [ "${ATOM_LATEST_VERSION}" != "${ATOM_CURRENT}" ]; then
         echo "upgrade atom ${ATOM_CURRENT} to ${ATOM_LATEST_VERSION}"
