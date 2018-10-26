81de9bedb11c5b2b60c5595447cc93a7ea7eee93
pascal <pascalito@gmail.com>
2018-10-26 10:04:55 +0200

    :bug: download x64
    WIP : download same build

--- zsh/functions/rambox.zsh
+++ zsh/functions/rambox.zsh
@@ -9,8 +9,8 @@ rambox_update(){
     rambox_CURRENT=$(rpm -aq | grep -i rambox | cut -d'-' -f2)
     rambox_LATEST=$(curl -Ls https://api.github.com/repos/saenzramiro/rambox/releases/latest)
     rambox_LATEST_VERSION=$(echo -e ${rambox_LATEST} | grep -m 1 '"name"'                          | cut -d '"' -f 4)
-    rambox_RPM=$(echo -e ${rambox_LATEST}            | grep -E 'browser_download_url(.*)\.rpm'     | cut -d '"' -f 4 | grep -v ia32)
-    rambox_DEB=$(echo -e ${rambox_LATEST}            | grep -E 'browser_download_url(.*)\.deb'     | cut -d '"' -f 4 | grep -v ia32) 
+    rambox_RPM=$(echo -e ${rambox_LATEST}            | grep -E 'browser_download_url(.*)\.rpm'     | cut -d '"' -f 4 | grep x86_64)
+    rambox_DEB=$(echo -e ${rambox_LATEST}            | grep -E 'browser_download_url(.*)\.deb'     | cut -d '"' -f 4 | grep amd64) 
 
     if [ "${rambox_LATEST_VERSION}" != "${rambox_CURRENT}" ]; then
         echo "upgrade rambox ${rambox_CURRENT} to ${rambox_LATEST_VERSION}"
