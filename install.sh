cda371161ef46f8ca4dcfbf9c41206b0a1d516f8
pascal <pascalito@gmail.com>
2018-12-17 11:31:29 +0100

    setup Flat-Remix theme

--- install.sh
+++ install.sh
@@ -38,19 +38,19 @@ done
 
 if which apt-get 1>/dev/null 2>&1 ; then
   if [ "$(id -u)" != "0" ]; then
-    sudo apt-get install python python-newt screenfetch
-  else
-    apt-get install python python-newt screenfetch
+    sudo add-apt-repository ppa:daniruiz/flat-remix
+    sudo apt-get install python python-newt screenfetch flat-remix-gnome flat-remix flat-remix-gtk
+  else 
+    add-apt-repository ppa:daniruiz/flat-remix
+    apt-get install python python-newt screenfetch flat-remix-gnome flat-remix flat-remix-gtk
   fi
 else
   if [ "$(id -u)" != "0" ]; then
-    sudo dnf copr enable tcg/themes
     sudo dnf copr enable daniruiz/flat-remix
-    sudo dnf install newt tilix-nautilus tilix numix-icon-theme-circle materia-theme screenfetch
+    sudo dnf install newt tilix-nautilus tilix screenfetch flat-remix-gnome flat-remix flat-remix-gtk
   else
-    dnf copr enable tcg/themes
     dnf copr enable daniruiz/flat-remix
-    dnf newt tilix-nautilus tilix numix-icon-theme-circle materia-theme screenfetch
+    dnf newt tilix-nautilus tilix screenfetch flat-remix-gnome flat-remix flat-remix-gtk
   fi
 fi
 vim +PluginInstall +qall
@@ -58,7 +58,8 @@ vim +PluginInstall +qall
 dconf load /com/gexperts/Tilix/ < tilix.dconf
 
 if which gsettings 1>/dev/null 2>&1 ; then
-  gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix";
-  gsettings set org.gnome.desktop.interface icon-theme "Numix"
-  gsettings set org.gnome.desktop.interface gtk-theme "Materia-compact"
+  gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix-Dark";
+  gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Dark"
+  gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Darker"
+
 fi
