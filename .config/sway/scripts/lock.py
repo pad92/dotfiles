a755fe63a405797ec1e83dbad27c3885d9fa8491
Pascal Armand <pascalito@gmail.com>
2019-03-24 22:27:38 +0100

    :sparkles: add pixelocker

---
+++ .config/sway/scripts/lock.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import subprocess
+import tempfile
+import os
+import sys
+
+import i3ipc.i3ipc
+i3 = i3ipc.Connection()
+outputs = [x['name'] for x in i3.get_outputs()]
+images = [tempfile.mktemp() for _ in outputs]
+lockcmd = ["swaylock"]
+
+# take the screenshots
+for output,image in zip(outputs, images):
+    subprocess.call(["grim", "-o", output, image])
+    subprocess.call(["convert", image, "-scale", "10%", "-scale", "1000%", image])
+    lockcmd.extend(["-F", "-c", "333333", "-s", "fill",  "-i", output+":"+image])
+
+# lock the screen
+lockcmd.extend(sys.argv[1:])
+subprocess.call(lockcmd)
+
+# cleanup
+for img in images:
+    os.remove(img)
