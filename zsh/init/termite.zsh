ec00f8d1431b08f503f969e8efc78e706ae8bd8b
pad <git@depad.fr>
2019-03-28 15:02:04 +0100

    :bug: fix source termit env

--- zsh/init/termite.zsh
+++ zsh/init/termite.zsh
@@ -1,5 +1,7 @@
 # allow termite to open new tabs on current directory
 if [[ "${TERM}" == "xterm-termite" ]]; then
- [[ -f "/etc/profile.d/vte.sh" ]] && source /etc/profile.d/vte.sh
-  __vte_osc7
+    if [[ -f "/etc/profile.d/vte.sh" ]]; then
+        source /etc/profile.d/vte.sh
+        __vte_osc7
+    fi
 fi
