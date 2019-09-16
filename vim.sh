9add5f21554da66adabd09eebf61a5990938a2f5
Pascal Armand <pascalito@gmail.com>
2019-09-16 15:21:56 +0200

    :sparkles: add vim install only

---
+++ vim.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# curl -sSL https://raw.githubusercontent.com/pad92/dotfiles/master/vim.sh | sh
+
+[ -e ~/.vim ]  && echo '~/.vim exist ' && exit 1
+[ -e ~/.vimrc ]  && echo '~/.vimrc exist ' && exit 1
+
+TMP_VIM=$(mktemp -u /tmp/vimrc.XXXXXX)
+git clone --recursive https://github.com/pad92/dotfiles.git "${TMP_VIM}"
+mv "${TMP_VIM}/.vim" ~/
+mv "${TMP_VIM}/.vimrc" ~/
+rm -fr "${TMP_VIM}/"
