f64f3b5ab161e93018c8852a33cd42285695eb4f
Pascal Armand <1067997+pad92@users.noreply.github.com>
2019-12-09 15:59:55 +0100

    :bug: fix resolution ( mbp )

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -9,13 +9,13 @@ HDMI2="${?}"
 if [ "${HDMI1}" -eq 0 ] && [ "${HDMI2}" -eq 0 ]; then
     # HDMI-2 HDMI-1
     #        eDP-1
-    xrandr --dpi 96 --output eDP-1 --primary --mode 1920x1080 --output HDMI-1 --auto --above eDP-1 --output HDMI-2 --auto --left-of HDMI-1 
+    xrandr --dpi 96 --output eDP-1 --primary --mode 1920x1200 --output HDMI-1 --auto --above eDP-1 --output HDMI-2 --auto --left-of HDMI-1 
 elif [ "${HDMI1}" -eq 0 ] && [ "${HDMI2}" -eq 1 ]; then
     # HDMI-1 eDP-1
-    xrandr --dpi 96 --output eDP-1 --primary --mode 1920x1080 --output HDMI-1 --auto --left-of eDP-1
+    xrandr --dpi 96 --output eDP-1 --primary --mode 1920x1200 --output HDMI-1 --auto --left-of eDP-1
 else
     # eDP-1
-    xrandr --dpi 96 --output eDP-1 --primary --mode 1920x1080
+    xrandr --dpi 96 --output eDP-1 --primary --mode 1920x1200
 fi
 
 feh --randomize --bg-scale ~/Pictures/Wallpapers/*.jpg
