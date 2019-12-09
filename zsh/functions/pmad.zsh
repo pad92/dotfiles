be64585e02920ad6f422fa9913853c96f127b024
pad <git@depad.fr>
2019-12-09 12:37:11 +0100

    clean

--- zsh/functions/pmad.zsh
+++ zsh/functions/pmad.zsh
@@ -1,32 +1,43 @@
 pmad() {
     if [ -z "$1" ]; then
-        echo 'Usage : pmad PORT USER [TUNNEL 80:127.0.0.1:80]'
+        echo 'Usage : pmad [PORT] [USER] [TUNNEL 80:127.0.0.1:80]'
     else
-        SSH_BIN=`which ssh`
-        SSH_IP='pmad'
-        SSH_PORT="$1"
-        SSH_CMD="$SSH_BIN -Y $SSH_IP -p $SSH_PORT"
-        if [ -n "$2" ]; then
-            SSH_CMD="$SSH_CMD -l $2"
+        SSH_PORT="${1}"
+        SSH_USER="${2}"
+        SSH_TUNNEL="${3}"
+        SSH_BIN=$(command -v ssh)
+        SSH_IP='pmad'                           # add pmad into /etc/hosts ;)
+        SSH_CMD="${SSH_BIN} -Y ${SSH_IP} -p ${SSH_PORT}"
+        if [ -x "${SSH_BIN}" ]; then
+            if [ -n "${SSH_USER}" ]; then
+                SSH_CMD="${SSH_CMD} -l ${SSH_USER}"
+            fi
+            if [ -n "$SSH_TUNNEL" ]; then
+                SSH_CMD="${SSH_CMD} -L ${SSH_TUNNEL}"
+            fi
+            $=SSH_CMD
+        else
+            echo 'ssh not found'
         fi
-        if [ -n "$3" ]; then
-            SSH_CMD="$SSH_CMD -L $3"
-        fi
-        $=SSH_CMD
     fi
 }
 pmad_push() {
-    if [ -z "$2" ]; then
-        echo 'Usage : pmad_push SSH_PUB PORT USER'
+    if [ -z "$1" ]; then
+        echo 'Usage : pmad_push [SSH_PUB] [PORT] [USER]'
     else
-        SSH_PUB=$1
-        PORT=$2
-        SSH_LOGIN=$3
-        if [ -f "$SSH_PUB" ]; then
-            IP='pmad'
-            ssh-copy-id -p $PORT -i $SSH_PUB $SSH_LOGIN@$IP
+        SSH_PUB="${1}"
+        PORT="${2}"
+        SSH_LOGIN="${3}"
+        SSH_BIN="$(command -v ssh-copy-id)"
+        if [ -x "${SSH_BIN}" ]; then
+            if [ -f "$SSH_PUB" ]; then
+                IP='pmad'
+                "${SSH_BIN}" -p "${PORT}" -i "${SSH_PUB}" "${SSH_LOGIN}@${IP}"
+            else
+                echo "${SSH_PUB} not found"
+            fi
         else
-            echo "$SSH_PUB not found"
+            echo 'ssh-copy-id not found'
         fi
     fi
 }
