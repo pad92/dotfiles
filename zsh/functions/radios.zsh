cbc076c4b144118e5531550e2fc2062bb921bada
pad <git@depad.fr>
2019-12-09 12:37:11 +0100

    clean

--- zsh/functions/radios.zsh
+++ zsh/functions/radios.zsh
@@ -1,14 +1,17 @@
 radio(){
-    if [ ${#} -ne 1 ]; then echo "usage: ${@} radio [radio name]"; fi
+    if [ ${#} -ne 1 ]; then
+        echo "usage: ${@} radio [radio name]"
+    fi
 
-    fg='http://radiofg.impek.com/fg'
+RADIO_FG='http://radiofg.impek.com/fg.mp3'
+RADIO_NOVA='http://novazz.ice.infomaniak.ch/novazz-128.mp3'
 
-    case "${1}" in
-        fg)
-            cvlc http://radiofg.impek.com/fg
-        ;;
-        nova)
-            cvlc http://broadcast.infomaniak.net/novazz-128.mp3 
-        ;;
-    esac
+case "${1}" in
+    fg)
+        cvlc "${RADIO_FG}"
+    ;;
+    nova)
+        cvlc "${RADIO_NOVA}"
+    ;;
+esac
 }
