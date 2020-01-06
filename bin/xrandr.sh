e7b80dcd2a9a14155ac80d67e203fa5c1b2fae03
pad <git@depad.fr>
2020-01-06 10:02:36 +0100

    :lipstick: fix dpi

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -9,7 +9,7 @@ HDMI2="${?}"
 if [ "${HDMI1}" -eq 0 ] && [ "${HDMI2}" -eq 0 ]; then
     # HDMI-2 HDMI-1
     #        eDP-1
-    xrandr --output eDP-1 --dpi 227 --primary --mode 2560x1600 --output HDMI-1 --auto --above eDP-1 --output HDMI-2 --auto --left-of HDMI-1 
+    xrandr --output eDP-1 --primary --mode 2560x1600 --output HDMI-1 --auto --above eDP-1 --output HDMI-2 --auto --left-of HDMI-1 
 elif [ "${HDMI1}" -eq 0 ] && [ "${HDMI2}" -eq 1 ]; then
     # HDMI-1 eDP-1
     xrandr --output eDP-1 --dpi 227 --primary --mode 2560x1600 --output HDMI-1 --auto --left-of eDP-1
