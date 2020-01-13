b074d6a3e2771e2ed4d6e37ea548919e92b799a0
pad <git@depad.fr>
2020-01-13 15:30:25 +0100

    :lipstick: fix resolutions

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -9,13 +9,25 @@ HDMI2="${?}"
 if [ "${HDMI1}" -eq 0 ] && [ "${HDMI2}" -eq 0 ]; then
     # HDMI-2 HDMI-1
     #        eDP-1
-    xrandr --output eDP-1 --primary --mode 2560x1600 --output HDMI-1 --auto --above eDP-1 --output HDMI-2 --auto --left-of HDMI-1 
+    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x1080 --rotate normal \
+           --output DP-1 --off \
+           --output DP-2 --off \
+           --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal \
+           --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal
 elif [ "${HDMI1}" -eq 0 ] && [ "${HDMI2}" -eq 1 ]; then
     # HDMI-1 eDP-1
-    xrandr --output eDP-1 --dpi 227 --primary --mode 2560x1600 --output HDMI-1 --auto --left-of eDP-1
+    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x1080 --rotate normal \
+           --output DP-1 --off \
+           --output DP-2 --off \
+           --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal \
+           --output HDMI-2 --off 
 else
     # eDP-1
-    xrandr --output eDP-1 --dpi 227 --primary --mode 2560x1600
+    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x1080 --rotate normal \
+           --output DP-1 --off \
+           --output DP-2 --off \
+           --output HDMI-1 --off \
+           --output HDMI-2 --off
 fi
 
 feh --randomize --bg-scale ~/Pictures/Wallpapers/*.jpg
