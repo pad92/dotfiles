34a465c5734c1abbdb41b9cdff3d35913d9e6d06
Pascal Armand <pascalito@gmail.com>
2021-03-15 23:40:22 +0100

    :pencil2: some fixs

--- bin/SystemControl.sh
+++ bin/SystemControl.sh
@@ -23,16 +23,17 @@ NETWORK="$(toggleNetwork)"
 INPUT=`echo -e "Lock\nLogout\nShutdown\nReboot\nSuspend\nScreen off\n$DPMS\n$NETWORK\nSwitch audio\nMute" | dmenu -i -nb '#C31616' -sb '#404040' -nf white`
 
 case "$INPUT" in
-	"Lock")         sleep 1; i3lock-fancy -p  -t '' ;;
-    "Logout")       i3exit logout ;;
-	"Shutdown")     i3exit shutdown ;;
-	"Reboot")       i3exit reboot ;;
-	"Suspend")      sleep 1; i3lock-fancy -p  -t '' && systemctl suspend ;;
-	"Screen off")   sleep 1; i3lock-fancy -p  -t '' && xset dpms force off ;;
-	"DPMS: on")     xset -dpms s off ;;
-	"DPMS: off")    xset +dpms s on ;;
-	"Network: on")  nmcli networking off ;;
-	"Network: off") nmcli networking on ;;
-	"Mute")         $HOME/.bin/ManageSound.sh "mute" ;;
-	"Switch audio") $HOME/.bin/ManageSound.sh "sw" ;;
+	"Lock")             sleep 1; i3lock-fancy -p  -t '' ;;
+    "Logout")           i3exit logout ;;
+	"Shutdown")         i3exit shutdown ;;
+	"Reboot")           i3exit reboot ;;
+	"Reboot Windows")   systemctl reboot --boot-loader-entry=auto-windows ;;
+	"Suspend")          sleep 1; i3lock-fancy -p  -t '' && systemctl suspend ;;
+	"Screen off")       sleep 1; i3lock-fancy -p  -t '' && xset dpms force off ;;
+	"DPMS: on")         xset -dpms s off ;;
+	"DPMS: off")        xset +dpms s on ;;
+	"Network: on")      nmcli networking off ;;
+	"Network: off")     nmcli networking on ;;
+	"Mute")             $HOME/.bin/ManageSound.sh "mute" ;;
+	"Switch audio")     $HOME/.bin/ManageSound.sh "sw" ;;
 esac
