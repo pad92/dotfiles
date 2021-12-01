7490c99fdca713f4da312465ef31b354db4f7bf3
Pascal Armand <pascalito@gmail.com>
2021-12-01 14:58:09 +0100

    lint files

--- .zshrc
+++ .zshrc
@@ -2,31 +2,31 @@ export ZSH=${HOME}/.dotfiles/zsh/
 export TERM=xterm-256color
 
 if [ -d ${HOME}/.bin ]; then
-    export PATH="${PATH}:${HOME}/.bin"
-    rehash
+  export PATH="${PATH}:${HOME}/.bin"
+  rehash
 fi
 
 if [ -d ${HOME}/.local/bin ]; then
-    export PATH="${PATH}:${HOME}/.local/bin"
-    rehash
+  export PATH="${PATH}:${HOME}/.local/bin"
+  rehash
 fi
 
 ZSH_THEME="pad"
 
 plugins=(
-    ansible
-    archlinux           # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/archlinux/README.md
-    command-not-found
-    docker
-    docker-compose
-    extract
-    github
-    httpie
-    rsync
-    zsh-autosuggestions
-    zsh-completions
-    zsh-syntax-highlighting
-    thefuck
+  ansible
+  archlinux           # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/archlinux/README.md
+  command-not-found
+  docker
+  docker-compose
+  extract
+  github
+  httpie
+  rsync
+  zsh-autosuggestions
+  zsh-completions
+  zsh-syntax-highlighting
+  thefuck
 )
 
 autoload -Uz colors && colors
@@ -61,7 +61,7 @@ setopt share_history
 setopt SHARE_HISTORY
 setopt APPEND_HISTORY
 
-[ -f "${HOME}/.config/user-dirs.dirs" ] && source ${HOME}/.config/user-dirs.dirs 
+[ -f "${HOME}/.config/user-dirs.dirs" ] && source ${HOME}/.config/user-dirs.dirs
 [ -f "${HOME}/.zshaliases" ]            && source ${HOME}/.zshaliases
 [ -f "${HOME}/.dir_colors" ]            && eval $(dircolors ${HOME}/.dir_colors)
 [ -x $(command -v neofetch) ]           && neofetch
--- bin/SystemControl.sh
+++ bin/SystemControl.sh
@@ -1,39 +1,48 @@
 #!/bin/bash
 
 function toggleDPMS() {
-	state="$(xset q | grep "DPMS is" | awk '{print $NF}')"
-	if [[ "$state" == "Enabled" ]]; then
-		echo "DPMS: on"
-	else
-    	echo "DPMS: off"
-    fi
+  state="$(xset q | grep "DPMS is" | awk '{print $NF}')"
+  if [[ "$state" == "Enabled" ]]; then
+    echo "DPMS: on"
+  else
+    echo "DPMS: off"
+  fi
 }
 
 function toggleNetwork() {
-	state="$(nmcli networking connectivity check)"
-	if [[ "$state" == "full" ]]; then
-		echo "Network: on"
-	else
-    	echo "Network: off"
-    fi
+  state="$(nmcli networking connectivity check)"
+  if [[ "$state" == "full" ]]; then
+    echo "Network: on"
+  else
+    echo "Network: off"
+  fi
 }
 
 DPMS="$(toggleDPMS)"
 NETWORK="$(toggleNetwork)"
-INPUT=`echo -e "Lock\nLogout\nShutdown\nReboot\nReboot Windows\nSuspend\nScreen off\n$DPMS\n$NETWORK\nSwitch audio\nMute" | dmenu -i -nb '#C31616' -sb '#404040' -nf white`
+INPUT=$(echo -e "Lock\nLogout\nShutdown\nReboot\nReboot Windows\nSuspend\nScreen off\n$DPMS\n$NETWORK\nSwitch audio\nMute" | dmenu -i -nb '#C31616' -sb '#404040' -nf white)
 
 case "$INPUT" in
-	"Lock")             sleep 1; i3lock-fancy -p  -t '' ;;
-    "Logout")           i3exit logout ;;
-	"Shutdown")         i3exit shutdown ;;
-	"Reboot")           i3exit reboot ;;
-	"Reboot Windows")   systemctl reboot --boot-loader-entry=auto-windows ;;
-	"Suspend")          sleep 1; i3lock-fancy -p  -t '' && systemctl suspend ;;
-	"Screen off")       sleep 1; i3lock-fancy -p  -t '' && xset dpms force off ;;
-	"DPMS: on")         xset -dpms s off ;;
-	"DPMS: off")        xset +dpms s on ;;
-	"Network: on")      nmcli networking off ;;
-	"Network: off")     nmcli networking on ;;
-	"Mute")             $HOME/.bin/ManageSound.sh "mute" ;;
-	"Switch audio")     $HOME/.bin/ManageSound.sh "sw" ;;
+"Lock")
+  sleep 1
+  i3lock-fancy -p -t ''
+  ;;
+"Logout") i3exit logout ;;
+"Shutdown") i3exit shutdown ;;
+"Reboot") i3exit reboot ;;
+"Reboot Windows") systemctl reboot --boot-loader-entry=auto-windows ;;
+"Suspend")
+  sleep 1
+  i3lock-fancy -p -t '' && systemctl suspend
+  ;;
+"Screen off")
+  sleep 1
+  i3lock-fancy -p -t '' && xset dpms force off
+  ;;
+"DPMS: on") xset -dpms s off ;;
+"DPMS: off") xset +dpms s on ;;
+"Network: on") nmcli networking off ;;
+"Network: off") nmcli networking on ;;
+"Mute") $HOME/.bin/ManageSound.sh "mute" ;;
+"Switch audio") $HOME/.bin/ManageSound.sh "sw" ;;
 esac
