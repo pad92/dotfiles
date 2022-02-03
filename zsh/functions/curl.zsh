99d41bb10fe7e6b2ba1bcf5b4317de71725f97c3
pad <git@depad.fr>
2022-02-03 17:01:42 +0100

    fix unit

--- zsh/functions/curl.zsh
+++ zsh/functions/curl.zsh
@@ -1,13 +1,13 @@
 curl_time() {
     CURL_FORMAT='                  http_code:  %{http_code}
-            time_namelookup:  %{time_namelookup} ms
-               time_connect:  %{time_connect} ms
-            time_appconnect:  %{time_appconnect} ms
-           time_pretransfer:  %{time_pretransfer} ms
-              time_redirect:  %{time_redirect} ms
-         time_starttransfer:  %{time_starttransfer} ms
+            time_namelookup:  %{time_namelookup}s
+               time_connect:  %{time_connect}s
+            time_appconnect:  %{time_appconnect}s
+           time_pretransfer:  %{time_pretransfer}s
+              time_redirect:  %{time_redirect}s
+         time_starttransfer:  %{time_starttransfer}s
                             ----------
-                 time_total:  %{time_total} ms\n'
+                 time_total:  %{time_total}s\n'
 
 	curl -w "$CURL_FORMAT" -o /dev/null -s $1
 }
