1896abaeaa52f1488d3e7bd8f33639dd21964b56
pad <git@depad.fr>
2022-06-09 09:42:22 +0200

    fixs

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -16,20 +16,20 @@ case "${LISTMONITORS}" in
     i3-msg "workspace 2, move workspace to output HDMI-0"
   fi
   ;;
-'DP1 DP3-1')
-  RES_DP1=$(xrandr --current | grep -A1 DP1 | tail -1 | awk '{print $1}')
-  RES_DP31=$(xrandr --current | grep -A1 DP3-1 | tail -1 | awk '{print $1}')
+'DP1-1 DP3 eDP1')
+  RES_DP11=$(xrandr --current | grep -A1 DP1-1 | tail -1 | awk '{print $1}')
+  RES_DP3=$(xrandr --current | grep -A1 DP3 | tail -1 | awk '{print $1}')
 
   # Home AOC +Benq / laptop docked on dell
-  if [ "x${RES_DP31}" = "x2560x1440" ] && [ "x${RES_DP1}" = "x1920x1080" ] && [ "x${LID_STATE}" = "xclosed" ]; then
+  #if [ "x${RES_DP3}" = "x2560x1440" ] && [ "x${RES_DP11}" = "x1920x1080" ] && [ "x${LID_STATE}" = "xclosed" ]; then
     xrandr --dpi 96 --output eDP1 --off \
-      --output DP1 --mode ${RES_DP1} --pos  0x0 --rotate right \
-      --output DP3-1 --primary --mode ${RES_DP31} --pos 1080x0 --rotate normal
-    i3-msg "workspace 1, move workspace to output DP3-1"
-    i3-msg "workspace 2, move workspace to output DP1"
+      --output DP1-1 --mode ${RES_DP11} --pos  0x0 --rotate right \
+      --output DP3 --primary --mode ${RES_DP3} --pos 1080x0 --rotate normal
+    i3-msg "workspace 1, move workspace to output DP3"
+    i3-msg "workspace 2, move workspace to output DP1-1"
     pkill xautolock ; xset s off ; xset -dpms ; xset s noblank
-    nmcli radio wifi off
-  fi
+    #nmcli radio wifi off
+  #fi
   ;;
 'eDP1')
   DPI=144
