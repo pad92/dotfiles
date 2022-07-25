4bf8f45db19c13ab85779273928dbaa8cea48eea
Pascal Armand <pascalito@gmail.com>
2022-07-25 11:36:36 +0200

    fix

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -4,6 +4,9 @@ LISTMONITORS=$(xrandr --current --listmonitors | grep -v '^Monitors' | awk '{pri
 
 [ -f /proc/acpi/button/lid/LID0/state ] && LID_STATE=$(grep -oE '[^ ]+$' /proc/acpi/button/lid/LID0/state)
 
+# 	xrandr --setprovideroutputsource modesetting NVIDIA-0
+
+
 case "${LISTMONITORS}" in
 'DP-3 HDMI-1')
   xrandr --dpi 96 \
@@ -29,12 +32,11 @@ case "${LISTMONITORS}" in
   ;;
 'DP-1-1 DP-3 eDP-1')
   xrandr --dpi 96 \
-    --output eDP-1 --auto --scale .5x.5 \
-    --output DP-3 --primary --auto --left-of eDP-1 \
+    --output eDP-1 --off \
+    --output DP-3 --primary --auto \
     --output DP-1-1 --auto --left-of DP-3 --rotate right
   i3-msg "workspace 1, move workspace to output DP-3"
   i3-msg "workspace 2, move workspace to output DP-1-1"
-  i3-msg "workspace 3, move workspace to output eDP-1"
   pkill xautolock ; xset s off ; xset -dpms ; xset s noblank
   ;;
 'eDP1')
