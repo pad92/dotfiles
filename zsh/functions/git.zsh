da3a489317ab284775c0795cdb97d54251c85de2
pad <git@depad.fr>
2022-11-22 13:05:57 +0100

    update recursive git prune

--- zsh/functions/git.zsh
+++ zsh/functions/git.zsh
@@ -1,6 +1,14 @@
-git_rp() {
-  for REPO in $(find ${PWD} -type d -name .git | sed 's@/.git@@g'); do
-    echo ${REPO}
-    GIT_PRUNE=$(git -C "${REPO}" remote prune origin)
+git_prune_r() {
+  for REPO in $(find $(pwd) -type d -name .git | sed 's@/.git@@g'); do
+    echo -ne "- ${REPO}"
+    PRUNE=$(git --git-dir=${REPO}/.git remote prune origin)
+    if [ $? -ne 0 ]; then
+      echo -ne "\tERROR\n"
+      echo "$(date +"%Y%m%d-%H%M") ERROR: ${REPO}" >> prune.log
+      echo "${PRUNE}"                              >> prune.log
+      echo ""                                      >> prune.log
+    else
+      echo -ne "\tOK\n"
+    fi
   done
 }
