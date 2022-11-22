0bcdff38b8ad088504f575b00213b689e463ce5c
Pascal Armand <pascalito@gmail.com>
2022-11-22 14:45:40 +0100

    fix output

--- zsh/functions/git.zsh
+++ zsh/functions/git.zsh
@@ -1,7 +1,7 @@
 git_prune_r() {
   for REPO in $(find $(pwd) -type d -name .git | sed 's@/.git@@g'); do
     echo -ne "- ${REPO}"
-    PRUNE=$(git --git-dir=${REPO}/.git remote prune origin)
+    PRUNE=$(git --git-dir=${REPO}/.git remote prune origin 2&>1 )
     if [ $? -ne 0 ]; then
       echo -ne "\tERROR\n"
       echo "$(date +"%Y%m%d-%H%M") ERROR: ${REPO}" >> prune.log
