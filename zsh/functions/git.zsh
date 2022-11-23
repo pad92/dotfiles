9943050cb4b811ef0cc16155f1179ab103af63f2
Pascal Armand <pascalito@gmail.com>
2022-11-23 14:43:39 +0100

    add recursive git pull

--- zsh/functions/git.zsh
+++ zsh/functions/git.zsh
@@ -1,11 +1,26 @@
 git_prune_r() {
   for REPO in $(find $(pwd) -type d -name .git | sed 's@/.git@@g'); do
     echo -ne "- ${REPO}"
-    PRUNE=$(git --git-dir=${REPO}/.git remote prune origin 2>&1 )
+    RET=$(git --git-dir=${REPO}/.git remote prune origin 2>&1 )
     if [ $? -ne 0 ]; then
       echo -ne "\tERROR\n"
       echo "$(date +"%Y%m%d-%H%M") ERROR: ${REPO}" >> prune.log
-      echo "${PRUNE}"                              >> prune.log
+      echo "${RET}"                                >> prune.log
+      echo ""                                      >> prune.log
+    else
+      echo -ne "\tOK\n"
+    fi
+  done
+}
+
+git_pull_r() {
+  for REPO in $(find $(pwd) -type d -name .git | sed 's@/.git@@g'); do
+    echo -ne "- ${REPO}"
+    RET=$(git --git-dir=${REPO}/.git pull 2>&1 )
+    if [ $? -ne 0 ]; then
+      echo -ne "\tERROR\n"
+      echo "$(date +"%Y%m%d-%H%M") ERROR: ${REPO}" >> prune.log
+      echo "${RET}"                                >> prune.log
       echo ""                                      >> prune.log
     else
       echo -ne "\tOK\n"
