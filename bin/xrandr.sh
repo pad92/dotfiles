31d75b51c6d6032202c1fa6180cd024ea945ebd7
Pascal Armand <1067997+pad92@users.noreply.github.com>
2023-02-09 13:58:05 +0100

    changing dockstation

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -7,39 +7,16 @@ LISTMONITORS=$(xrandr --current --listmonitors | grep -v '^Monitors' | awk '{pri
 # 	xrandr --setprovideroutputsource modesetting NVIDIA-0
 
 case "${LISTMONITORS}" in
-'DP-3 HDMI-1')
-  xrandr --dpi 96 \
-    --output HDMI-1 --auto --pos 0x0 --rotate right \
-    --output DP-3 --primary --auto --pos 1080x0 --rotate normal
-  i3-msg "workspace 1, move workspace to output DP-3"
-  i3-msg "workspace 2, move workspace to output HDMI-1"
-  pkill xautolock
-  ;;
-'DP1-1 DP3 eDP1')
-  #xrandr --setprovideroutputsource modesetting NVIDIA-0
- # xrandr --dpi 96 \
- #   --output eDP1 --auto --scale .5x.5 \
- #   --output DP3 --primary --auto --left-of eDP1 \
- #   --output DP1-1 --auto --left-of DP3 --rotate right
-  xrandr --dpi 96 \
-    --output eDP1 --off \
-    --output DP3 --primary --auto  \
-    --output DP1-1 - -auto --left-of DP3 --rotate right
-  i3-msg "workspace 1, move workspace to output DP3"
-  i3-msg "workspace 2, move workspace to output DP1-1"
-  #i3-msg "workspace 3, move workspace to output eDP1"
-  pkill xautolock ; xset s off ; xset -dpms ; xset s noblank
-  ;;
-'DP-1 DP-3-1 eDP-1')
+'DP-1-1 DP-1-2 eDP-1')  # anker @ home
   xrandr --dpi 96 \
     --output eDP-1 --off \
-    --output DP-1 --primary --auto \
-    --output DP-3-1 --auto --left-of DP-1 --rotate right
-  i3-msg "workspace 1, move workspace to output DP-1"
-  i3-msg "workspace 2, move workspace to output DP-3-1"
-  pkill xautolock ; xset s off ; xset -dpms ; xset s noblank
+    --output DP-1-1 --auto --pos 0x0 --rotate right \
+    --output DP-1-2 --primary --auto --pos 1080x0 --rotate normal
+  i3-msg "workspace 1, move workspace to output DP-1-2"
+  i3-msg "workspace 2, move workspace to output DP-1-1"
+  pkill xautolock
   ;;
-'eDP1')
+'eDP1')                 # alone
   DPI=144
   echo "Xft.dpi: ${DPI}" | xrdb -merge
   xrandr --dpi ${DPI}
