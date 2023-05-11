2f8e88dfdb6d7704df445aa5925cefd57fe4b3c6
Pascal Armand <pascalito@gmail.com>
2023-05-11 17:18:30 +0200

    backup with rsync/ssh

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 . /etc/os-release
 
-MNT_PTS='/mnt/pads918/home'
-BACKUP_DIR="${MNT_PTS}/${USERNAME}/backup"
+BACKUP_DIR="pascal@192.168.0.100:backup"
 HOSTNAME="$(hostnamectl hostname)"
 
+BASEDIR=$(dirname "$0")
 TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
 RSYNC_OPTS='--archive --perms --xattrs --safe-links --no-specials --no-devices --info=progress2 --delete --delete-excluded'
 SUDO_OPTS='-E -s'
-if [ ! -d "${BACKUP_DIR}/${HOSTNAME}${HOME}/" ]; then
-  echo "create ${BACKUP_DIR}/${HOSTNAME}${HOME}/"
-  sudo mkdir -p "${BACKUP_DIR}/${HOSTNAME}${HOME}/"
-fi
-PKGLIST_OLD="$(sudo ls -1 ${BACKUP_DIR}/${HOSTNAME}/pkglist-*.txt 2>/dev/null | tail -1)"
-PKGLIST_CURRENT="${BACKUP_DIR}/${HOSTNAME}/pkglist-${TIMESTAMP}-${ID}.txt"
+#if [ ! -d "${BACKUP_DIR}/${HOSTNAME}${HOME}/" ]; then
+#  echo "create ${BACKUP_DIR}/${HOSTNAME}${HOME}/"
+#  sudo mkdir -p "${BACKUP_DIR}/${HOSTNAME}${HOME}/"
+#fi
+PKGLIST_OLD="$(ls -1 ${BASEDIR}/../dist/${ID}/pkglist-*.txt 2>/dev/null | tail -1)"
+PKGLIST_CURRENT="${BASEDIR}/../dist/${ID}/pkglist-${TIMESTAMP}-${ID}.txt"
+
+echo "PKGLIST_OLD : ${PKGLIST_OLD}"
+echo "PKGLIST_CURRENT : ${PKGLIST_CURRENT}"
 
 [ -z "${ID_LIKE}" ] && ID_LIKE="${ID}"
 
@@ -28,12 +31,12 @@ arch)
 
   yay -Scc --noconfirm
 
-  pacman -Qqe | awk '{print $1}' | sudo tee -a ${PKGLIST_CURRENT} 1>/dev/null
+  pacman -Qqe | awk '{print $1}' | tee -a ${PKGLIST_CURRENT} 1>/dev/null
 
   ;;
 debian)
   sudo apt autoremove --purge
-  dpkg --get-selections '*' | sudo tee -a ${PKGLIST_CURRENT} 1>/dev/null
+  dpkg --get-selections '*' | tee -a ${PKGLIST_CURRENT} 1>/dev/null
 
   ;;
 
@@ -44,14 +47,14 @@ debian)
 esac
 
 if [ -s "${PKGLIST_OLD}" ]; then
-  diff -U 0 "${PKGLIST_OLD}" "${PKGLIST_CURRENT}" > "${BACKUP_DIR}/${HOSTNAME}/pkglist-${TIMESTAMP}.diff"
-  if [ -s "${BACKUP_DIR}/${HOSTNAME}/pkglist-${TIMESTAMP}.diff" ]; then
-    cat "${BACKUP_DIR}/${HOSTNAME}/pkglist-${TIMESTAMP}.diff"
+  diff -U 0 "${PKGLIST_OLD}" "${PKGLIST_CURRENT}" > "${BASEDIR}/../dist/${ID}/pkglist-${TIMESTAMP}.diff"
+  if [ -s "${BASEDIR}/../dist/${ID}/pkglist-${TIMESTAMP}.diff" ]; then
+    cat "${BASEDIR}/../dist/${ID}/pkglist-${TIMESTAMP}.diff"
   else
-    rm -f "${BACKUP_DIR}/${HOSTNAME}/pkglist-${TIMESTAMP}.diff"
+    rm -f "${BASEDIR}/../dist/${ID}/pkglist-${TIMESTAMP}.diff"
   fi
 fi
 
-sudo rsync ${RSYNC_OPTS} ${HOME}/ ${BACKUP_DIR}/${HOSTNAME}${HOME}/ --exclude-from=- <<- EOF
+sudo ${SUDO_OPTS} rsync ${RSYNC_OPTS} ${HOME}/ ${BACKUP_DIR}/${HOSTNAME}${HOME}/ --exclude-from=- <<- EOF
 $(cat ${HOME}/.no_backup.txt ${HOME}/.dotfiles/dist/backup_excludes.txt)
 EOF
