d461c6a36c0c8c1eab61f22062efa840e29c7c0d
pad <git@depad.fr>
2025-05-19 09:25:53 +0200

    update config and functions/plugins

--- zsh/functions/crypt.zsh
+++ zsh/functions/crypt.zsh
@@ -3,5 +3,5 @@ sha1()   { echo -n $1 | openssl sha1 /dev/stdin }
 sha256() { echo -n $1 | openssl dgst -sha256 /dev/stdin }
 sha512() { echo -n $1 | openssl dgst -sha512 /dev/stdin }
 
-urlencode() { python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])" "$@" }
-urldecode() { python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])" "$@" }
+#urlencode() { python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])" "$@" }
+#urldecode() { python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])" "$@" }
