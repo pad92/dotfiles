3533ac8f4d0ee8ea45e5e67e44f45d9a736e7c19
pad <git@depad.fr>
2026-02-28 16:56:16 +0100

    add Multi hosts

--- bin/swww.sh
+++ bin/swww.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 WALLPAPER_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/backgrounds"
-export SWWW_TRANSITION_FPS=60
-export SWWW_TRANSITION_STEP=2
+export SWWW_TRANSITION_FPS=1
+export SWWW_TRANSITION_STEP=255
 
 # Get the names of all monitors.
 MONITORS=$(swww query | grep -Po "^[^:]+")
 MONITORS=$(swww query | sed -E 's/^: ([^:]+):.*/\1/')
 
-for MONITOR in $MONITORS; do
+for MONITOR in ${MONITORS}; do
   # Find a random image file in the wallpaper directory.
   IMAGE=$(find -L "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*.png" \) | shuf -n 1)
 
   # Set the image as the wallpaper for the current monitor.
-  swww img --outputs "$MONITOR" "$IMAGE" --resize=crop --transition-type random
+  swww img --outputs "${MONITOR}" "${IMAGE}" --resize=crop --transition-type simple
 done
